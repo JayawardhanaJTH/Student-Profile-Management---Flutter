bb483658a2e9024f08f2e12dbe592af85f0e7556
JayawardanaJTH <tiran2323@gmail.com>
2022-04-06 21:39:29 +0530

    create registration method

--- student_profile_management/lib/pages/registration_page.dart
+++ student_profile_management/lib/pages/registration_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:student_profile_management/models/student.dart';
+import 'package:student_profile_management/models/teacher.dart';
 import 'package:student_profile_management/pages/widgets/header_page.dart';
+import 'package:student_profile_management/services/studentService.dart';
+import 'package:student_profile_management/services/teacherService.dart';
+import 'package:toast/toast.dart';
 
 import 'common/theme_helper.dart';
 
@@ -19,6 +24,40 @@ class _RegistrationPageState extends State<RegistrationPage> {
   final _formKey = GlobalKey<FormState>();
   bool checkedValue = false;
   bool checkboxValue = false;
+
+  String? userType = "Student";
+  String? _userName;
+  String? _name;
+  String? _email;
+  String? _password;
+  String? _contact;
+
+  void onSubmit() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      if (userType == "Student") {
+        await StudentService().addStudent(Student(null, userType!,
+            name: _name!,
+            userName: _userName!,
+            email: _email!,
+            password: _password!));
+
+        Toast.show("Student Added", context,
+            gravity: Toast.CENTER, duration: Toast.LENGTH_LONG);
+      } else {
+        await TeacherService().addTeacher(Teacher(null, userType!,
+            name: _name!,
+            userName: _userName!,
+            email: _email!,
+            password: _password!,
+            contact: _contact!));
+
+        Toast.show("Teacher Added", context,
+            gravity: Toast.CENTER, duration: Toast.LENGTH_LONG);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,6 +124,18 @@ class _RegistrationPageState extends State<RegistrationPage> {
                           child: TextFormField(
                             decoration: ThemeHelper().textInputDecoration(
                                 'Full Name', 'Enter your full name'),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter your name";
+                              }
+                              return null;
+                            },
+                            onSaved: (String? value) {
+                              setState(() {
+                                _name = value;
+                              });
+                            },
+                            
                           ),
                           decoration: ThemeHelper().inputBoxDecorationShaddow(),
                         ),
@@ -95,6 +146,17 @@ class _RegistrationPageState extends State<RegistrationPage> {
                           child: TextFormField(
                             decoration: ThemeHelper().textInputDecoration(
                                 'User Name', 'Enter your user name'),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter your user name";
+                              }
+                              return null;
+                            },
+                            onSaved: (String? value) {
+                              setState(() {
+                                _userName = value;
+                              });
+                            },
                           ),
                           decoration: ThemeHelper().inputBoxDecorationShaddow(),
                         ),
@@ -105,12 +167,17 @@ class _RegistrationPageState extends State<RegistrationPage> {
                                 "E-mail address", "Enter your email"),
                             keyboardType: TextInputType.emailAddress,
                             validator: (val) {
-                              if (!(val!.isEmpty) &&
+                              if ((val == null) ||
                                   !RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
                                       .hasMatch(val)) {
                                 return "Enter a valid email address";
                               }
                               return null;
+                            },
+                            onSaved: (String? value) {
+                              setState(() {
+                                _email = value;
+                              });
                             },
                           ),
                           decoration: ThemeHelper().inputBoxDecorationShaddow(),
@@ -127,25 +194,88 @@ class _RegistrationPageState extends State<RegistrationPage> {
                               }
                               return null;
                             },
-                          ),
-                          decoration: ThemeHelper().inputBoxDecorationShaddow(),
-                        ),
-                        const SizedBox(height: 20.0),
-                        Container(
-                          child: TextFormField(
-                            decoration: ThemeHelper().textInputDecoration(
-                                "Mobile Number", "Enter your mobile number"),
-                            keyboardType: TextInputType.phone,
-                            validator: (val) {
-                              if (!(val!.isEmpty) &&
-                                  !RegExp(r"^(\d+)*$").hasMatch(val)) {
-                                return "Enter a valid phone number";
-                              }
-                              return null;
+                            onSaved: (String? value) {
+                              setState(() {
+                                _password = value;
+                              });
                             },
                           ),
                           decoration: ThemeHelper().inputBoxDecorationShaddow(),
                         ),
+                        const SizedBox(height: 20.0),
+                        const Text(
+                          "Select user type",
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 20.0),
+                        FormField<bool>(
+                          builder: (state) {
+                            return Column(
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Radio<String>(
+                                      value: "Student",
+                                      groupValue: userType,
+                                      onChanged: (String? value) {
+                                        setState(() {
+                                          userType = value;
+                                        });
+                                      },
+                                    ),
+                                    const Text(
+                                      "Student",
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    const SizedBox(
+                                      width: 50,
+                                    ),
+                                    Radio<String>(
+                                      value: "Teacher",
+                                      groupValue: userType,
+                                      onChanged: (String? value) {
+                                        setState(() {
+                                          userType = value;
+                                        });
+                                      },
+                                    ),
+                                    const Text(
+                                      "Teacher",
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 20.0),
+                        userType == "Teacher"
+                            ? Container(
+                                child: TextFormField(
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      "Mobile Number",
+                                      "Enter your mobile number"),
+                                  keyboardType: TextInputType.phone,
+                                  validator: (val) {
+                                    if ((val == null) ||
+                                        (val.isEmpty) ||
+                                        !RegExp(r"^(\d+)*$").hasMatch(val)) {
+                                      return "Enter a valid phone number";
+                                    }
+                                    return null;
+                                  },
+                                  onSaved: (String? value) {
+                                    setState(() {
+                                      _contact = value;
+                                    });
+                                  },
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              )
+                            : const SizedBox(height: 1.0),
                         const SizedBox(height: 15.0),
                         FormField<bool>(
                           builder: (state) {
@@ -208,14 +338,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
                               ),
                             ),
                             onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                // Navigator.of(context).pushAndRemoveUntil(
-                                //     MaterialPageRoute(
-                                //         builder: (context) => ProfilePage()
-                                //     ),
-                                //         (Route<dynamic> route) => false
-                                // );
-                              }
+                              onSubmit();
                             },
                           ),
                         ),
